acb9a2fff603589fabd52263af9a5010b75a2ce4
zarunpb <sarunpb@hotmail.com>
2025-03-07 10:51:33 +0700

    first draft

---
+++ day10_winform/day10_winform/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace day10_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.question_text = new System.Windows.Forms.TextBox();
+            this.solution_textbox = new System.Windows.Forms.TextBox();
+            this.solution_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(585, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "คำนวณ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // question_text
+            // 
+            this.question_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question_text.Location = new System.Drawing.Point(83, 76);
+            this.question_text.Multiline = true;
+            this.question_text.Name = "question_text";
+            this.question_text.Size = new System.Drawing.Size(462, 79);
+            this.question_text.TabIndex = 1;
+            // 
+            // solution_textbox
+            // 
+            this.solution_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solution_textbox.ForeColor = System.Drawing.Color.Teal;
+            this.solution_textbox.Location = new System.Drawing.Point(83, 249);
+            this.solution_textbox.Multiline = true;
+            this.solution_textbox.Name = "solution_textbox";
+            this.solution_textbox.Size = new System.Drawing.Size(179, 58);
+            this.solution_textbox.TabIndex = 2;
+            this.solution_textbox.Text = "0";
+            this.solution_textbox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // solution_label
+            // 
+            this.solution_label.AutoSize = true;
+            this.solution_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solution_label.ForeColor = System.Drawing.Color.Red;
+            this.solution_label.Location = new System.Drawing.Point(385, 252);
+            this.solution_label.Name = "solution_label";
+            this.solution_label.Size = new System.Drawing.Size(42, 46);
+            this.solution_label.TabIndex = 3;
+            this.solution_label.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 566);
+            this.Controls.Add(this.solution_label);
+            this.Controls.Add(this.solution_textbox);
+            this.Controls.Add(this.question_text);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TextCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox question_text;
+        private System.Windows.Forms.TextBox solution_textbox;
+        private System.Windows.Forms.Label solution_label;
+    }
+}
+
---
+++ day10_winform/day10_winform/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day10_winform
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string gettext = question_text.Text;
+            DataTable table = new DataTable();
+            var c = table.Compute(gettext, null);
+            solution_textbox.Text = c.ToString();
+            solution_label.Text = c.ToString();
+        }
+    }
+}
